17bb0657f60d233575e98d15b74381ceb989e265
yunz <112053367+yunz-dev@users.noreply.github.com>
2024-07-25 09:55:38 +1000

    removed wallpapers

--- .config/hypr/start.sh
+++ .config/hypr/start.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
-# initialise wallpaper
-swww init &
-# set wallpaper
-swww img ~/Downloads/wallpaper.jpg &
+# # initialise wallpaper
+# swww init &
+# # set wallpaper
+# swww img ~/Downloads/wallpaper.jpg &
+# swww img -o DP-5 ~/Downloads/wallpaper.jpg &
+# swww img -o DP-7 ~/Downloads/wallpaper.jpg &
 #
 nm-applet --indicator &
 
