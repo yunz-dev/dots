4ccc871c082a54cbe66b4c6644bf6251f9cf97fc
yunz <112053367+yunz-dev@users.noreply.github.com>
2024-08-06 10:42:38 +1000

    kanata: added

--- .config/hypr/start.sh
+++ .config/hypr/start.sh
@@ -3,10 +3,14 @@
 # # initialise wallpaper
 # swww init &
 # # set wallpaper
+# swww img ~/Downloads/wallpapers/cactus.jpg &
 # swww img ~/Downloads/wallpaper.jpg &
 # swww img -o DP-5 ~/Downloads/wallpaper.jpg &
-# swww img -o DP-7 ~/Downloads/wallpaper.jpg &
+# swww img -o DP-7 ~/Downloads/wallpapers/cactus.jpg &
+# swww img -o DP-8 ~/Downloads/wallpapers/cactus.jpg &
+# swww img -o DP-9 ~/Downloads/wallpapers/cactus.jpg &
 #
 nm-applet --indicator &
-
+# doesnt work, do it properly
+# sudo kanata -c ~/dots/.config/kanata/config.kbd &
 waybar &
